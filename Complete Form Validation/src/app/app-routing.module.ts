7b21accef22b25dc0e0c95f0dc158c20b5210020
RehanSk93 <skazam1993@gmail.com>
2019-11-14 18:44:04 +0530

    Complete Form validation

---
+++ Complete Form Validation/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TemplateDrivenFormComponent } from './form-component/template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './form-component/reactive-form/reactive-form.component';
+import { FormArrayComponent } from './form-component/form-array/form-array.component';
+
+const routes: Routes = [
+  { path: 'template-driven-form', component: TemplateDrivenFormComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path: 'form-array', component: FormArrayComponent },
+  { path: '', redirectTo: '/template-driven-form', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
